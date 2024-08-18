4252b3b3f8f75b0c1a27dc1e42c21413bfed581d
Andrew Davidson <andrew.davidson.digital@gmail.com>
2024-08-18 20:33:32 +1000

    Structure:  - mocking compoents

--- src/views/Projects.vue
+++ src/views/Projects.vue
@@ -7,9 +7,39 @@ import VisualHighlight from '../components/VisualHighlight.vue';
 
 <template>
   <section class="bg-black text-white min-h-[100%]">
-    <VisualHighlight copy="foo bar baz" colour="red" />
-    <VisualHighlight copy="thingi-2" colour="blue" leftAligned />
-    <VisualHighlight copy="another one" colour="red" />
+    <VisualHighlight 
+      title="Internal Warehouse & Order Managment System"
+      subTitle="Past Work"
+      copy=""
+      :media="{
+        src: '', 
+        type: 'image',
+      }"
+      :tags="[
+        'React', 
+        'NextJS', 
+        'Typescript',
+        'CMS',
+      ]"
+      colour="blue"
+      leftAligned
+    />
+    <VisualHighlight 
+      title="Visual Novel Game Engine"
+      subTitle="Personal Project"
+      copy="foo bar baz" 
+      :media="{
+        src: '/video/bg-menu.mp4', 
+        type: 'video',
+      }"
+      :tags="[
+        'Vue', 
+        'Typescript',
+        'Scripting',
+        'Gaming',
+      ]"
+      colour="red"
+    />
   </section>
 </template>
 
