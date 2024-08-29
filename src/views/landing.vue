e4487cab9a1054ff9e23bf166ab7f5bd02e4f040
Andrew Davidson <andrew.davidson.digital@gmail.com>
2024-08-29 17:35:13 +1000

    Structure:  - Add support for Past-Work client-2  - Ignore working files

--- src/views/landing.vue
+++ src/views/landing.vue
@@ -65,8 +65,8 @@
       subTitle="Past Work"
       copy="Development and rollout of Atomic Design, from CMS authoring down to React rendering and deployment" 
       :media="{
-        src: '', 
-        type: 'image',
+        src: ['/image/marketing_2_0.webp', '/image/marketing_2_1.webp'], 
+        type: 'imageCollection',
       }"
       :tags="[
         'React', 
