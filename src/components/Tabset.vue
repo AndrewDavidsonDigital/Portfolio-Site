8d34289e3c97cdc4044d3b37a2a7d69870378e6b
Andrew Davidson <andrew.davidson.digital@gmail.com>
2024-09-01 14:43:46 +1000

    Structure:  - Add support for tab-sets  - re-structure content collection pages

---
+++ src/components/Tabset.vue
@@ -0,0 +1,21 @@
+<script setup lang="ts">
+  export interface ITab {
+    key: string;
+    title: string;
+  };
+  interface Props {
+    tabs: ITab[];
+  }
+
+  const props = defineProps<Props>();
+  ;
+</script>
+
+<template>
+  <section class="flex gap-4">
+    <template v-for="(tab, index) in props.tabs" :key="`tabIndex_${index}`">
+      <div @click="$emit('setActive', tab.key)">{{ tab.title }}</div>
+    </template>
+  </section>
+  <slot></slot>
+</template>
--- src/views/Projects.vue
+++ src/views/Projects.vue
@@ -1,45 +1,46 @@
 
 <script setup lang="ts">
-import VisualHighlight from '../components/VisualHighlight.vue';
+  import VisualHighlight from '@components/VisualHighlight.vue';
+  import Tabset, { ITab } from '@components/Tabset.vue';
+  import PWAck from '@content/PW-ack.vue';
+  import PRIllusion from '@content/PR-illusion.vue';
+  import { ref } from 'vue';
+
+  const tabs: ITab[] = [
+    { 
+      title: 'Game Engine',
+      key: 'illusionEngine',
+    },
+  ]
+
+  const activeKey = ref<string>(tabs[0].key);
 
 
 </script>
 
 <template>
-  <section class="bg-black text-white min-h-[100%]">
-    <VisualHighlight 
-      title="Internal Warehouse & Order Managment System"
-      subTitle="Past Work"
-      copy=""
-      :media="{
-        src: '', 
-        type: 'image',
-      }"
-      :tags="[
-        'React', 
-        'NextJS', 
-        'Typescript',
-        'CMS',
-      ]"
-      colour="blue"
-      leftAligned
-    />
-    <VisualHighlight 
-      title="Visual Novel Game Engine"
-      subTitle="Personal Project"
-      copy="foo bar baz" 
-      :media="{
-        src: '/video/bg-menu.mp4', 
-        type: 'video',
-      }"
-      :tags="[
-        'Vue', 
-        'Typescript',
-        'Scripting',
-        'Gaming',
-      ]"
-      colour="red"
-    />
+  <section class="bg-black text-white min-h-[100%] !justify-start">
+    <Tabset 
+      :tabs="tabs"
+      @setActive="(key: string) => activeKey = key"
+    >
+      <div class="grid-area-stack">
+        <TransitionGroup>
+          <PRIllusion v-if="activeKey === tabs[0].key"/>
+        </TransitionGroup>
+      </div>
+    </Tabset>
   </section>
 </template>
 
+<style scoped>
+  .v-enter-active,
+  .v-leave-active {
+    transition: opacity 1s ease;
+  }
+
+  .v-enter-from,
+  .v-leave-to {
+    opacity: 0;
+  }
+</style>
