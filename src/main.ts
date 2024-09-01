590904a36b71aa08183aa7b7e781b7175ff4cd21
Andrew Davidson <andrew.davidson.digital@gmail.com>
2024-09-02 01:29:26 +1000

    Structure:  - Add support for code highliting

--- src/main.ts
+++ src/main.ts
@@ -5,10 +5,14 @@ import './style.css'
 import App from './App.vue'
 import router from './router'
 
+import 'highlight.js/styles/stackoverflow-dark.css'
+import 'highlight.js/lib/common';
 
+import hljsVuePlugin from "@highlightjs/vue-plugin";
 
 const app = createApp(App)
 
 app.use(router)
+app.use(hljsVuePlugin)
 
 app.mount('#app');
