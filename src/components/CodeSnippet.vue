14f1a448fdf7bdba0d0ec1a4c76f799ed886c3b8
Andrew Davidson <andrew.davidson.digital@gmail.com>
2024-09-04 11:18:51 +1000

    Structure:  - resolve overflow issue for code, due to addition on caption support  - lock mobile size to ~95% screen width

--- src/components/CodeSnippet.vue
+++ src/components/CodeSnippet.vue
@@ -11,7 +11,7 @@
 </script>
 
 <template>
-  <section class="flex flex-col items-center w-screen px-2 pb-2 md:w-fit md:max-w-[60rem] mx-auto overflow-hidden [&>pre>code]:whitespace-pre-wrap [&>pre>code]:rounded-lg">
+  <section class="flex flex-col items-center w-[95dvw] py-2 md:w-fit md:max-w-[60rem] mx-auto overflow-hidden [&>pre>code]:whitespace-pre-wrap [&>pre>code]:rounded-lg [&>pre]:w-[inherit]">
     <highlightjs :autodetect="props?.language ? false : true" :code="props.content" :language="props.language"/>
     <div v-if="props?.caption" class="italic font-normal text-center text-balance">{{ props.caption }}</div>
   </section>
