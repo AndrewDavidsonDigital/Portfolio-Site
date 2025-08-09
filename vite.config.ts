200b458e5400276d3bf5bf8618dec8370709d95c
Andrew Davidson <andrew.davidson.digital@gmail.com>
2025-08-09 10:36:34 +1000

    DX: Add chunking as threeJS is really big ~1mb un-compressed so spereate both vender / three / app

--- vite.config.ts
+++ vite.config.ts
@@ -6,6 +6,7 @@ import path from "path";
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  plugins: [vue()],
   base: './',
   publicDir: "public",
   resolve: {
@@ -16,9 +17,26 @@ export default defineConfig({
       "@assets": path.resolve(__dirname, "./src/assets"),
       "@lib": path.resolve(__dirname, "./src/lib"),
       "@router": path.resolve(__dirname, "./src/router"),
-      // "@stores": path.resolve(__dirname, "./src/stores"),    
-      // "&audio": path.resolve(__dirname, "./src/stores/audio"),      
     },
   },
-  plugins: [vue()],
+  build: {
+    chunkSizeWarningLimit: 1000,
+    rollupOptions: {
+      output: {
+        manualChunks
+      }
+    }
+  }
 })
+
+
+function manualChunks(id: string) {
+	if (id.includes('node_modules') && id.includes('/three/')) {
+		return 'three';
+	}
+	if (id.includes('node_modules')) {
+		return 'vendor';
+	}
+
+	return null;
+}
